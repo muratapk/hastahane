fd87e14778b1d5f1180881aad55348c16acd50f9
muratapk <muratapk@hotmail.com>
2023-08-20 16:29:37 +0300

    yeni hesap oluşturuldu

---
+++ hastakayit/kutuphane.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+using Microsoft.VisualBasic;
+namespace hastakayit
+{
+    public class kutuphane
+    {
+        private string baglanti = "Data Source=.;Initial Catalog=hastane;Integrated Security=True";
+        public SqlConnection con;
+        public SqlCommand cmd;
+        public SqlDataAdapter da;
+        public DataTable dt;
+        public DataSet ds;
+
+        public void openConnection()
+        {
+            try
+            {
+                con = new SqlConnection(this.baglanti);
+                if(con.State==ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                MessageBox.Show("Hata Oluştu"+Ex.Message);
+            }
+        }
+
+        public void closeConnection()
+        {
+            try
+            {
+                con = new SqlConnection(this.baglanti);
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                MessageBox.Show("Hata Oluştu" + Ex.Message);
+            }
+        }
+        public DataTable doldur(string cumle)
+        {
+            this.cmd = new SqlCommand(cumle, this.con);
+            this.da = new SqlDataAdapter(this.cmd);
+            this.dt = new DataTable();
+            this.da.Fill(this.dt);
+            return this.dt;
+
+        }
+
+        public void crud(string cumle)
+        {
+            try
+            {
+                this.cmd = new SqlCommand(cumle, this.con);
+                this.cmd.ExecuteNonQuery();
+                MessageBox.Show("İşlem Başarılı Şekilde Tamamlandı");
+            }
+            catch (Exception Ex)
+            {
+
+                MessageBox.Show("Hata Oluştu" + Ex.Message);
+            }
+           
+        }
+
+
+    }
+}
---
+++ hastakayit/sehirler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hastakayit
+{
+    public partial class sehirler : Form
+    {
+        public sehirler()
+        {
+            InitializeComponent();
+        }
+        kutuphane veri = new kutuphane();
+        private void sehirler_Load(object sender, EventArgs e)
+        {
+            veri.openConnection();
+        }
+
+        private void sehirler_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            veri.closeConnection();
+        }
+    }
+}
